4e12c932ba3568cbd352485c0bd73827b4d132a0
Jacob Schwarz <jschwarz@220OFFICE.local>
2018-07-03 16:33:01 -0700

    Code updates

--- main.go
+++ main.go
@@ -9,12 +9,13 @@ const (
 	IADD
 	IMUL
 	ISUB
+	IDIV
 	IGT
 	ILT
 	IET
 	JMP
-	BRT
-	BRF
+	JNZ
+	JZ
 	MSTORE
 	MLOAD
 	LOAD
@@ -23,7 +24,7 @@ const (
 	CPRINT
 	CALL
 	RET
-	HALT
+	EXIT
 )
 
 // booleans
@@ -54,31 +55,44 @@ func (vm *VM) Cpu() {
 			vm.stack = append(vm.stack, v)
 
 		case IADD:
-			v1 := vm.stack[len(vm.stack)-1]
-			vm.stack = vm.stack[:len(vm.stack)-1]
-
 			v2 := vm.stack[len(vm.stack)-1]
 			vm.stack = vm.stack[:len(vm.stack)-1]
 
-			vm.stack = append(vm.stack, v1+v2)
+			v1 := vm.stack[len(vm.stack)-1]
+			vm.stack = vm.stack[:len(vm.stack)-1]
+
+			vm.stack = append(vm.stack, v1 + v2)
 
 		case IMUL:
-			v1 := vm.stack[len(vm.stack)-1]
-			vm.stack = vm.stack[:len(vm.stack)-1]
-
 			v2 := vm.stack[len(vm.stack)-1]
 			vm.stack = vm.stack[:len(vm.stack)-1]
 
-			vm.stack = append(vm.stack, v1*v2)
+			v1 := vm.stack[len(vm.stack)-1]
+			vm.stack = vm.stack[:len(vm.stack)-1]
+
+			vm.stack = append(vm.stack, v1 * v2)
 
 		case ISUB:
-			v1 := vm.stack[len(vm.stack)-1]
+			v2 := vm.stack[len(vm.stack)-1]
 			vm.stack = vm.stack[:len(vm.stack)-1]
 
-			v2 := vm.stack[len(vm.stack)-1]
+			v1 := vm.stack[len(vm.stack)-1]
 			vm.stack = vm.stack[:len(vm.stack)-1]
 
 			vm.stack = append(vm.stack, v1-v2)
+
+		case IDIV:
+			v2 := vm.stack[len(vm.stack)-1]
+			vm.stack = vm.stack[:len(vm.stack)-1]
+
+			v1 := vm.stack[len(vm.stack)-1]
+			vm.stack = vm.stack[:len(vm.stack)-1]
+
+			quotient := int(v1 / v2)
+			remainder := int(v1 % v2)
+
+			vm.stack = append(vm.stack, remainder)
+			vm.stack = append(vm.stack, quotient)
 
 		case MSTORE:
 			addr := vm.code[vm.ip]
@@ -143,7 +157,7 @@ func (vm *VM) Cpu() {
 		case JMP:
 			vm.ip = vm.code[vm.ip]
 
-		case BRT:
+		case JNZ:
 			addr := vm.code[vm.ip]
 			vm.ip++
 			if vm.stack[len(vm.stack)-1] == TRUE {
@@ -151,7 +165,7 @@ func (vm *VM) Cpu() {
 			}
 			vm.stack = vm.stack[:len(vm.stack)-1]
 
-		case BRF:
+		case JZ:
 			addr := vm.code[vm.ip]
 			vm.ip++
 			if vm.stack[len(vm.stack)-1] == FALSE {
@@ -198,7 +212,7 @@ func (vm *VM) Cpu() {
 			vm.stack = vm.stack[:len(vm.stack)-nargs]
 			vm.stack = append(vm.stack, retv)
 
-		case HALT:
+		case EXIT:
 			break
 		}
 
@@ -211,103 +225,12 @@ func (vm *VM) Cpu() {
 
 func main() {
 	code := []int{
-		// fibonacci(n)
-		// if n == 0: return 0
-		ICONST, // 0
-		0,      // 1
-		LOAD,   // 2
-		-3,     // 3
-		IET,    // 4
-		BRT,    // 5
-		32,     // 6
-
-		// if n == 1: return 1
-		ICONST, // 7
-		1,      // 8
-		LOAD,   // 9
-		-3,     // 10
-		IET,    // 11
-		BRT,    // 12
-		35,     // 13
-
-		// v1 := fib(n-1)
-		ICONST, // 14
-		1,      // 15
-		LOAD,   // 16
-		-3,     // 17
-		ISUB,   // 18
-		CALL,   // 19
-		0,      // 20
-		1,      // 21
-
-		// v2 := fib(n-2)
-		ICONST, // 22
-		2,      // 23
-		LOAD,   // 24
-		-3,     // 25
-		ISUB,   // 26
-		CALL,   // 27
-		0,      // 28
-		1,      // 29
-
-		// return v1 + v2
-		IADD, // 30
-		RET,  // 31
-
-		// return 0
-		ICONST, // 32
-		0,      // 33
-		RET,    // 34
-
-		// return 1
-		ICONST, // 35
-		1,      // 36
-		RET,    // 37
-
-		// main()
-		// i := 0
-		ICONST, // 38
-		0,      // 39
-		MSTORE, // 40
-		0,      // 41
-
-		// while i < 10 {
-		ICONST, // 42
-		21,     // 43
-		MLOAD,  // 44
-		0,      // 45
-		ILT,    // 46
-		BRF,    // 47
-		64,     // 48
-
-		// fibonacci(n)
-		MLOAD,  // 49
-		0,      // 50
-		CALL,   // 51
-		0,      // 52
-		1,      // 53
-		IPRINT, // 54
-
-		// i++
-		MLOAD,  // 55
-		0,      // 56
-		ICONST, // 57
-		1,      // 58
-		IADD,   // 59
-		MSTORE, // 60
-		0,      // 61
-
-		// }
-		JMP, // 62
-		42,  // 63
-
-		// }
-		HALT, // 64
+		
 	}
 
 	// cpu setup
-	entry := 38
-	memsize := 1
+	entry := 0
+	memsize := 2
 	debug := false
 
 	vm := VM{
